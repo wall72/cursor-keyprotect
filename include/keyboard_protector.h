06e6194d700eb2142d0b64ec4050b2f3923127fe
Cliff W. Lee <changho.lee@gmail.com>
2025-10-30 15:43:58 +0900

    ?ㅻ낫???낅젰 ?뷀샇??湲곕뒫 異붽? 諛?Doxygen 二쇱꽍 ?곸슜
    - crypto_keycode.c 異붽?: ??肄붾뱶 ?뷀샇??蹂듯샇???⑥닔 援ы쁽
    
    - ?ㅻ낫???낅젰???뷀샇?뷀븯??異쒕젰?섎룄濡?蹂寃?(李⑤떒?섏? ?딄퀬 ?꾨떖)
    
    - 紐⑤뱺 ?⑥닔 諛??꾩뿭 蹂?섏뿉 Doxygen 二쇱꽍 異붽?

--- include/keyboard_protector.h
+++ include/keyboard_protector.h
@@ -5,12 +5,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// 함수 선언
+/**
+ * @brief 키 코드를 솔트와 키를 사용하여 암호화합니다.
+ * @param key_code 암호화할 키 코드
+ * @param salt 암호화에 사용할 솔트 값 (매번 다르게 생성 권장)
+ * @return unsigned int 암호화된 키 코드
+ */
+unsigned int encrypt_keycode_with_salt(unsigned int key_code, unsigned int salt);
+
+/**
+ * @brief 암호화된 키 코드를 솔트와 키를 사용하여 복호화합니다.
+ * @param encrypted_code 복호화할 암호화된 키 코드
+ * @param salt 암호화에 사용했던 동일한 솔트 값
+ * @return unsigned int 복호화된 (원래의) 키 코드
+ */
+unsigned int decrypt_keycode_with_salt(unsigned int encrypted_code, unsigned int salt);
+
+/**
+ * @brief 키보드 입력이 발생할 때마다 호출되는 저수준 키보드 후크 프로시저
+ * @param nCode 후크 프로시저가 메시지를 처리할지 다음 프로시저로 전달할지 결정하는 코드
+ * @param wParam 메시지 타입 (WM_KEYDOWN, WM_KEYUP, WM_SYSKEYDOWN, WM_SYSKEYUP)
+ * @param lParam KBDLLHOOKSTRUCT 구조체에 대한 포인터
+ * @return LRESULT 메시지를 차단하려면 0이 아닌 값을 반환, 전달하려면 CallNextHookEx의 반환값을 반환
+ */
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam);
+
+/**
+ * @brief 키보드 후크를 설치하고 설정하는 함수
+ * @details 관리자 권한을 확인하고 WH_KEYBOARD_LL 저수준 키보드 후크를 시스템 전역에 설치합니다.
+ */
 void SetHook(void);
+
+/**
+ * @brief 설치된 키보드 후크를 해제하는 함수
+ */
 void UnsetHook(void);
 
-// 전역 변수 선언
+/**
+ * @brief 전역 키보드 후크 핸들
+ */
 extern HHOOK g_keyboardHook;
 
 #endif // KEYBOARD_PROTECTOR_H
--- src/keyboard_protector.c
+++ src/keyboard_protector.c
@@ -1,10 +1,23 @@
 #include "keyboard_protector.h"
 
-// 전역 변수: 후크 핸들 및 실행 상태 플래그
+/**
+ * @brief 전역 키보드 후크 핸들
+ */
 HHOOK g_keyboardHook = NULL;
+
+/**
+ * @brief 프로그램 실행 상태 플래그
+ * @details TRUE일 때 프로그램이 실행 중이며, FALSE로 설정되면 종료됩니다.
+ */
 volatile BOOL g_running = TRUE;
 
-// 후크 프로시저: 키보드 입력이 발생할 때마다 호출됨
+/**
+ * @brief 키보드 입력이 발생할 때마다 호출되는 저수준 키보드 후크 프로시저
+ * @param nCode 후크 프로시저가 메시지를 처리할지 다음 프로시저로 전달할지 결정하는 코드
+ * @param wParam 메시지 타입 (WM_KEYDOWN, WM_KEYUP, WM_SYSKEYDOWN, WM_SYSKEYUP)
+ * @param lParam KBDLLHOOKSTRUCT 구조체에 대한 포인터
+ * @return LRESULT 메시지를 차단하려면 0이 아닌 값을 반환, 전달하려면 CallNextHookEx의 반환값을 반환
+ */
 LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     // nCode가 0보다 작으면 시스템에서 후크를 처리해야 함
     if (nCode >= 0) {
@@ -14,9 +27,6 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
             // 키보드 데이터 구조체 포인터
             KBDLLHOOKSTRUCT* pKbdStruct = (KBDLLHOOKSTRUCT*)lParam;
             
-            // 1. 키보드 입력 감지 및 처리
-            printf("[키 감지] KeyCode: %lu\n", pKbdStruct->vkCode);
-
             // Esc 키 (VK_ESCAPE)는 종료를 위해 예외적으로 허용
             if (pKbdStruct->vkCode == VK_ESCAPE) {
                 printf("\n[알림] Esc 키가 눌렸습니다. 후크를 해제하고 종료합니다.\n");
@@ -27,11 +37,21 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
                 return CallNextHookEx(g_keyboardHook, nCode, wParam, lParam);
             }
 
-            // 2. 키 입력 차단 로직 (핵심)
-            printf("[차단] 키 입력이 성공적으로 차단되었습니다. (다음 앱으로 전달되지 않음)\n");
+            // 원본 키 코드
+            unsigned int original_keycode = (unsigned int)pKbdStruct->vkCode;
             
-            // 1을 리턴하여 메시지를 '흡수'하고 다음 애플리케이션으로 전달되는 것을 차단합니다.
-            return 1; 
+            // 솔트 생성: 현재 시간(밀리초)을 기반으로 생성
+            unsigned int salt = GetTickCount();
+            
+            // 키 코드 암호화
+            unsigned int encrypted_keycode = encrypt_keycode_with_salt(original_keycode, salt);
+            
+            // 암호화된 키 코드 출력
+            printf("[키 감지] 원본 KeyCode: %u | 솔트: %u | 암호화된 KeyCode: %u\n", 
+                   original_keycode, salt, encrypted_keycode);
+            
+            // 키 입력을 차단하지 않고 암호화 정보를 출력한 후 원래대로 전달
+            return CallNextHookEx(g_keyboardHook, nCode, wParam, lParam); 
         }
     }
     
@@ -39,7 +59,11 @@ LRESULT CALLBACK LowLevelKeyboardProc(int nCode, WPARAM wParam, LPARAM lParam) {
     return CallNextHookEx(g_keyboardHook, nCode, wParam, lParam);
 }
 
-// 후크를 설정하는 함수
+/**
+ * @brief 키보드 후크를 설치하고 설정하는 함수
+ * @details 관리자 권한을 확인하고 WH_KEYBOARD_LL 저수준 키보드 후크를 시스템 전역에 설치합니다.
+ *          Windows XP 호환성을 위해 관리자 권한 확인은 Windows Vista 이상에서만 수행됩니다.
+ */
 void SetHook() {
     // 관리자 권한 확인 (선택사항, Windows 7 이상에서만 동작)
     // TOKEN_ELEVATION은 Windows Vista 이상에서만 지원됨
@@ -89,7 +113,7 @@ void SetHook() {
         exit(1);
     }
     
-    printf("[성공] 키보드 보안 툴이 실행되었습니다. 모든 키 입력이 차단됩니다.\n");
+    printf("[성공] 키보드 보안 툴이 실행되었습니다. 모든 키 입력이 암호화되어 출력됩니다.\n");
     if (isAdmin) {
         printf("[정보] 관리자 권한으로 실행 중입니다.\n");
     }
@@ -97,7 +121,9 @@ void SetHook() {
     printf("Esc 키를 눌러 종료하십시오.\n\n");
 }
 
-// 후크를 해제하는 함수
+/**
+ * @brief 설치된 키보드 후크를 해제하는 함수
+ */
 void UnsetHook() {
     if (g_keyboardHook != NULL) {
         UnhookWindowsHookEx(g_keyboardHook);
